3e6b05906a55e396fba331d40f219f95b037336a
lolacmartinlopez <103166690+lolacmartinlopez@users.noreply.github.com>
2022-04-19 14:18:57 +0200

    Copia funcionamiento correcto licencia

--- StartOpenness/Program.cs
+++ StartOpenness/Program.cs
@@ -3,7 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using Siemens.Engineering; 
+using Siemens.Engineering;
+using Siemens.Engineering.Cax;
+using Siemens.Engineering.Compiler;
+using Siemens.Engineering.Compare;
+using Siemens.Engineering.Download;
+using Siemens.Engineering.Hmi;
+using Siemens.Engineering.Hmi.Cycle;
+using Siemens.Engineering.Hmi.Communication;
+using Siemens.Engineering.Hmi.Globalization;
+using Siemens.Engineering.Hmi.RuntimeScripting;
+using Siemens.Engineering.Hmi.Screen;
+using Siemens.Engineering.Hmi.Tag;
+using Siemens.Engineering.Hmi.TextGraphicList;
+using Siemens.Engineering.HW;
+using Siemens.Engineering.HW.Extensions;
+using Siemens.Engineering.HW.Features;
+using Siemens.Engineering.HW.Utilities;
+using Siemens.Engineering.Library;
+using Siemens.Engineering.Library.MasterCopies;
+using Siemens.Engineering.Library.Types;
+using Siemens.Engineering.SW;
+using Siemens.Engineering.SW.Blocks;
+using Siemens.Engineering.SW.ExternalSources;
+using Siemens.Engineering.SW.Tags;
+using Siemens.Engineering.SW.TechnologicalObjects;
+using Siemens.Engineering.SW.TechnologicalObjects.Motion;
+using Siemens.Engineering.SW.Types;
+using Siemens.Engineering.Upload;
+
 
 namespace StartOpenness
 {
